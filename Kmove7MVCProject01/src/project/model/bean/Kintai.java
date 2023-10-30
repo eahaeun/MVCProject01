d6c6c9fa18ed872a608cde1ce88ab03c674b1df6
minssseoni <148943913+minssseoni@users.noreply.github.com>
2023-10-30 18:25:20 +0900

    근태 model, service.request 작성

---
+++ Kmove7MVCProject01/src/project/model/bean/Kintai.java
@@ -0,0 +1,47 @@
+package project.model.bean;
+
+import java.util.Date;
+
+public class Kintai {
+
+	private String SHAIN_NO;
+	private String KINTAI_KM;
+	private Date NYUROKU_YMD;
+	private Date KAISHI_YMD;
+	private Date SHURYO_YMD;
+	private int KINTAI_PAY;
+	
+	public Kintai(String SHAIN_NO, String KINTAI_KM, Date NYUROKU_YMD, Date KAISHI_YMD, Date SHURYO_YMD, int KINTAI_PAY) {
+		this.SHAIN_NO = SHAIN_NO;
+		this.KINTAI_KM = KINTAI_KM;
+		this.NYUROKU_YMD = NYUROKU_YMD;
+		this.KAISHI_YMD = KAISHI_YMD;
+		this.SHURYO_YMD = SHURYO_YMD;
+		this.KINTAI_PAY = KINTAI_PAY;
+	}
+
+	public String getSHAIN_NO() {
+		return SHAIN_NO;
+	}
+
+	public String getKINTAI_KM() {
+		return KINTAI_KM;
+	}
+
+	public Date getNYUROKU_YMD() {
+		return NYUROKU_YMD;
+	}
+
+	public Date getKAISHI_YMD() {
+		return KAISHI_YMD;
+	}
+
+	public Date getSHURYO_YMD() {
+		return SHURYO_YMD;
+	}
+
+	public int getKINTAI_PAY() {
+		return KINTAI_PAY;
+	}
+	
+}
---
+++ Kmove7MVCProject01/src/project/model/service/KintaiRequest.java
@@ -0,0 +1,55 @@
+package project.model.service;
+
+import java.sql.Date;
+import java.util.Map;
+
+public class KintaiRequest {
+	private String SHAIN_NO;
+	private String KINTAI_KM;
+	private Date NYUROKU_YMD;
+	private Date KAISHI_YMD;
+	private Date SHURYO_YMD;
+	private int KINTAI_PAY;
+	
+	public KintaiRequest(String SHAIN_NO, String KINTAI_KM, Date NYUROKU_YMD, Date KAISHI_YMD, Date SHURYO_YMD, int KINTAI_PAY) {
+		this.SHAIN_NO = SHAIN_NO;
+		this.KINTAI_KM = KINTAI_KM;
+		this.NYUROKU_YMD = NYUROKU_YMD;
+		this.KAISHI_YMD = KAISHI_YMD;
+		this.SHURYO_YMD = SHURYO_YMD;
+		this.KINTAI_PAY = KINTAI_PAY;
+	}
+
+	public String getSHAIN_NO() {
+		return SHAIN_NO;
+	}
+
+	public String getKINTAI_KM() {
+		return KINTAI_KM;
+	}
+
+	public Date getNYUROKU_YMD() {
+		return NYUROKU_YMD;
+	}
+
+	public Date getKAISHI_YMD() {
+		return KAISHI_YMD;
+	}
+
+	public Date getSHURYO_YMD() {
+		return SHURYO_YMD;
+	}
+
+	public int getKINTAI_PAY() {
+		return KINTAI_PAY;
+	}
+	
+	public void validate(Map<String,Boolean>errors) {
+		if(SHAIN_NO == null || SHAIN_NO.trim().isEmpty()) {
+			errors.put("SHAIN_NO", Boolean.TRUE);
+		}
+	}
+	
+
+}
+
