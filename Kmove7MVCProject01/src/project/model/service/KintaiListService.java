121440cd10886795e305e842a77be5ffa5f5402d
minssseoni <148943913+minssseoni@users.noreply.github.com>
2023-11-02 19:45:27 +0900

    수정

--- Kmove7MVCProject01/src/project/model/service/KintaiListService.java
+++ Kmove7MVCProject01/src/project/model/service/KintaiListService.java
@@ -10,21 +10,37 @@ import project.dao.ShainDao;
 import project.model.bean.Shain;
 
 public class KintaiListService {
-	ShainDao ShainDao = new ShainDao();
-		
-		public List<Shain> getList() throws SQLException {
-			Connection conn = null;
-			
-			try {
-				conn = ConnectionProvider.getConnection();
-				conn.setAutoCommit(false);
-				List<Shain> ticketList = ShainDao.select(conn);
-				return ticketList;
-			} catch(SQLException e) {
-				JdbcUtil.rollback(conn);
-				return null;
-			} finally {
-				JdbcUtil.close(conn);
-			}
+ShainDao shainDao = new ShainDao();
+	
+	public List<Shain> getShainList() throws SQLException{
+		Connection conn = null;
+		try {
+			conn = ConnectionProvider.getConnection();
+			conn.setAutoCommit(false);
+			List<Shain> shainList = shainDao.select(conn);
+			conn.commit();
+			return shainList;
+		}catch(SQLException e) {
+			JdbcUtil.rollback(conn);
+			return null;
+		}finally {
+			JdbcUtil.close(conn);
 		}
+	}
+	//재직
+	public List<Shain> getRetShainList() throws SQLException{
+		Connection conn = null;
+		try {
+			conn = ConnectionProvider.getConnection();
+			conn.setAutoCommit(false);
+			List<Shain> shainList = shainDao.retselect(conn);
+			conn.commit();
+			return shainList;
+		}catch(SQLException e) {
+			JdbcUtil.rollback(conn);
+			return null;
+		}finally {
+			JdbcUtil.close(conn);
+		}
+	}
 }
